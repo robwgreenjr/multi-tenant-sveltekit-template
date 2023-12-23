7e26a2b25075d74eb38a7889a8091b43dd644617
Rob <rob.green@greenbeard.io>
2023-12-23 08:50:54 -0500

    have backend and internal app start when docker containers start

--- docker/start.sh
+++ docker/start.sh
@@ -2,4 +2,19 @@
 
 service nginx restart
 
+curl -s "https://get.sdkman.io" | bash
+source /root/.sdkman/bin/sdkman-init.sh \
+    && sdk install java 21.0.1-amzn \
+    && sdk use java 21.0.1-amzn \
+    && sdk install maven \
+    && cd /opt/multi-tenant-spring-template \
+    && mvn spring-boot:run &
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.nvm/nvm.sh \
+        && nvm install --lts \
+        && nvm use --lts \
+        && cd /opt/internal-app \
+        && npm run dev &
+
 tail -f /dev/null
